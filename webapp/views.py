668e6e26bb68057a1e7587401aadb81e92b9d2d3
kkkk <doexx82@gmail.com>
2019-01-16 13:14:03 +0100

    La mia app Django Girls, primo commit

---
+++ webapp/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h2>HEY!</h2>")
+
+
+def index(request):
+    return HttpResponse("<h2>HEY!</h2>")
